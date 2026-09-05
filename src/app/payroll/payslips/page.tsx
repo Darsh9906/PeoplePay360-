37d99c7b42f62d1def834bb5d696b2746d996b38
dhrumil911 <dhrumildholakiya12@gmail.com>
2026-09-05 21:18:40 +0530

    feat: add payslips pages

---
+++ src/app/payroll/payslips/page.tsx
@@ -0,0 +1,5 @@
+import Payslips from "@/src/pages/payroll/Payslips"
+
+export default function PayslipsPage() {
+  return <Payslips />
+}
--- src/pages/payroll/PayslipDetails.jsx
+++
@@ -1,3 +0,0 @@
-export default function PayslipDetails() {
-  return <main><h1>Payslip details</h1></main>;
-}
